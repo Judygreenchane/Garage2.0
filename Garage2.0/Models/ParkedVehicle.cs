12b092c88b9ff7d85a0564db49650bcc5ae1b0df
Reza Zam <rezazam_skol@outlook.com>
2024-10-30 10:16:35 +0100

    Created  Scaffolded_Controller

--- Garage2.0/Models/ParkedVehicle.cs
+++ Garage2.0/Models/ParkedVehicle.cs
@@ -6,10 +6,10 @@ namespace Garage2._0.Models
     {
         public int Id { get; set; }
         public VehicleType VehicleType { get; set; }
-        public string RegistrationNumber { get; set; }
-        public string Color { get; set; }
-        public string Brand { get; set; }
-        public string VehicleModel { get; set; }
+        public string? RegistrationNumber { get; set; }
+        public string? Color { get; set; }
+        public string? Brand { get; set; }
+        public string? VehicleModel { get; set; }
         public int Wheel { get; set; }
         public DateTime ArrivalTime { get; set; }
 
