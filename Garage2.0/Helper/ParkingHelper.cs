ee1c7526ccda56baff06886a99b594f0432081c7
Deddel <83078233+deddel@users.noreply.github.com>
2024-10-31 13:39:26 +0100

    Added ParkingHelper Changed ReceiptView to format ParkedTime and ParkingFee

---
+++ Garage2.0/Helper/ParkingHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace Garage2._0.Helper
+{
+    public class ParkingHelper  
+    {
+        public const decimal ParkingRate = 0.5m; // Amount per minute
+
+        public static string FormatTimeSpan(TimeSpan timeSpan)
+        {
+            return $"{timeSpan.Days} days {timeSpan.Hours} hours {timeSpan.Minutes} minutes";
+        }
+
+        public static decimal ParkingFee(DateTime arrival, DateTime departure)
+        {
+            decimal fee = Math.Round((decimal)(departure-arrival).TotalMinutes*ParkingRate, 2);
+            return fee;
+        }
+    }
+}
